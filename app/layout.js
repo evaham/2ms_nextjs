f65819349de7bc129ef9acec4c61424564ca581f
JungWoo Ham <evaham@naver.com>
2025-09-05 09:03:40 +0900

    뷰포트 메타 태그 추가: 반응형 디자인 지원을 위한 설정

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body style={{height: "100%"}}>
         <style>
           {`
