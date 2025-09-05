4e2b6210005cfb3f2a7e24b4f3cbf4db0c6ced06
JungWoo Ham <evaham@naver.com>
2025-09-05 09:48:36 +0900

    뷰포트 메타 태그 제거: 불필요한 코드 정리

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body style={{height: "100%"}}>
         <style>
