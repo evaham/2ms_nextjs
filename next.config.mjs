fd02f7e3af81f176c482a70a0c38605c21ed59df
JungWoo Ham <evaham@naver.com>
2026-02-03 15:10:03 +0900

    feat: next.config.mjs에 basePath 및 assetPrefix 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const repoName = "2ms_nextjs";
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+	basePath: isProd ? `/${repoName}` : "",
+	assetPrefix: isProd ? `/${repoName}` : "",
+};
 
 export default nextConfig;
