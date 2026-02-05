9acc67e4f2af5cec42ff24b55a43fb834dfbc6cd
JungWoo Ham <evaham@naver.com>
2026-02-05 10:16:59 +0900

    fix: 수정된 광고 배너의 토글 버튼 위치 및 스타일 조정

--- app/component/FloatingAdBanner.js
+++ app/component/FloatingAdBanner.js
@@ -14,23 +14,12 @@ export default function FloatingAdBanner({
       <div
         className="relative transition-transform duration-300"
         style={{
-          transform: isOpen ? "translateX(0)" : "translateX(calc(100% - 1rem))",
+          transform: isOpen ? "translateX(0)" : "translateX(calc(100% - -1px))",
         }}
       >
-        <div className="relative flex items-center h-26 px-1 py-1 border border-slate-600 rounded bg-white shadow-lg">
-          <button
-            type="button"
-            onClick={onToggle}
-            className="absolute -left-6 flex items-center h-26 bg-amber-500 text-sm font-bold text-white"
-            aria-label="플로팅 광고 토글"
-          >
-            {isOpen ? 
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#333"><path d="M320-200v-560l440 280-440 280Z"/></svg> 
-              : <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#333"><path d="M640-200 200-480l440-280v560Z"/></svg>
-            }
-          </button>
+        <div className="relative flex items-center py-px pr-px bg-slate-400 shadow-lg">
           <a href={banner.link} className="flex items-center ">
-            <div className="flex w-75 h-26 items-center justify-center overflow-hidden bg-amber-50">
+            <div className="flex w-56 h-19 items-center justify-center overflow-hidden bg-white">
               <img
                 src={`${process.env.NEXT_PUBLIC_BASE_PATH || ""}${banner.image}`}
                 alt="광고 배너"
@@ -38,6 +27,19 @@ export default function FloatingAdBanner({
               />
             </div>
           </a>
+          <button
+            type="button"
+            onClick={onToggle}
+            className="absolute -left-6 flex items-center justify-center w-6 h-full rounded-tl-lg rounded-bl-lg bg-slate-400 text-sm font-bold text-white shadow"
+            aria-label="플로팅 광고 토글"
+          >
+            {isOpen ? 
+              <svg xmlns="http://www.w3.org/2000/svg" className="size-4.5 fill-neutral-700" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M320-200v-560l440 280-440 280Z"/></svg> 
+              : <svg xmlns="http://www.w3.org/2000/svg" className="size-4.5 fill-neutral-700" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M640-200 200-480l440-280v560Z"/></svg>
+            }
+          </button>
+
+
         </div>
       </div>
     </div>
